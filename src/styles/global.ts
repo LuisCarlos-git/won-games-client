0951c1736471b966a0e7d2de777c641fe848ecb6
LuisCarlos-git <luisestrela.bol951@gmail.com>
2021-04-05 09:36:05 -0300

    styles(GlobalStyles): remove background on storybook

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,16 @@
-import { createGlobalStyle, css } from 'styled-components';
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components';
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStylesProps = { removeBg?: boolean };
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
   /* poppins-300 - latin */
 @font-face {
   font-family: 'Poppins';
@@ -35,16 +45,25 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::after,&::before {
+      box-sizing: inherit;
+    }
   }
 
 
-  ${({ theme }) => css`
+  ${({ theme, removeBg }) => css`
     html {
       font-size: 62.5%;
     }
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+
+      ${!removeBg &&
+      css`
+        background-color: ${theme.colors.mainBg};
+      `}
     }
   `}
 
