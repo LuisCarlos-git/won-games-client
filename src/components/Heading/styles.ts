ae93c3961728cdae917ce4666a5edced386dc56f
Luis Carlos <luisestrela.bol951@gmail.com>
2021-08-10 16:08:13 -0300

    export heading modifiers

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -2,7 +2,7 @@ import media from 'styled-media-query';
 import styled, { css, DefaultTheme } from 'styled-components';
 import { HeadingProps, LineColors } from '.';
 
-const wrapperModifiers = {
+export const wrapperModifiers = {
   small: (theme: DefaultTheme) => css`
     font-size: ${theme.font.sizes.medium};
 
