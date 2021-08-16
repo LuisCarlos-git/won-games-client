859bcbe8ae242b913b10d4b5f9e7837b01bbf868
Luis Carlos <luisestrela.bol951@gmail.com>
2021-08-16 09:57:34 -0300

    profile menu

---
+++ src/components/ProfileMenu/index.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import {
+  AccountCircle,
+  CreditCard,
+  ExitToApp,
+  FormatListBulleted
+} from 'styled-icons/material-outlined';
+import * as S from './styles';
+
+export type ProfileMenuProps = {
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders';
+};
+
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
+  <S.Nav>
+    <Link href="/profile/me" passHref>
+      <S.Link isActive={activeLink === '/profile/me'}>
+        <AccountCircle size={24} />
+        <span>My profile</span>
+      </S.Link>
+    </Link>
+    <Link href="/profile/cards" passHref>
+      <S.Link isActive={activeLink === '/profile/cards'}>
+        <CreditCard size={24} />
+        <span>My cards</span>
+      </S.Link>
+    </Link>
+
+    <Link href="/profile/orders" passHref>
+      <S.Link isActive={activeLink === '/profile/orders'}>
+        <FormatListBulleted size={24} />
+        <span>My orders</span>
+      </S.Link>
+    </Link>
+
+    <Link href="/profile/logout" passHref>
+      <S.Link>
+        <ExitToApp size={24} />
+        <span>Sign out</span>
+      </S.Link>
+    </Link>
+  </S.Nav>
+);
+
+export default ProfileMenu;
---
+++ src/components/ProfileMenu/stories.tsx
@@ -0,0 +1,11 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import ProfileMenu, { ProfileMenuProps } from '.';
+
+export default {
+  title: 'ProfileMenu',
+  component: ProfileMenu
+} as Meta;
+
+export const Default: Story<ProfileMenuProps> = (args) => (
+  <ProfileMenu {...args} />
+);
