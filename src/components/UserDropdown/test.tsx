ac941ced350bd9ea3248a4657ba873bd868a98d9
Luis Carlos <luisestrela.bol951@gmail.com>
2021-08-23 14:21:40 -0300

    ajust tests

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -1,15 +1,21 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithTheme } from 'utils/tests/helper';
 
 import UserDropdown from '.';
 
 describe('<UserDropdown />', () => {
-  it('should render the links', () => {
-    renderWithTheme(<UserDropdown />);
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="luis carlos" />);
 
-    expect(screen.getByText(/my profile/i)).toBeInTheDocument();
-    expect(screen.getByText(/wishlist/i)).toBeInTheDocument();
-    expect(screen.getByText(/sign out/i)).toBeInTheDocument();
+    userEvent.click(screen.getByText(/luis carlos/i));
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument();
   });
 
   it('should render the username is correctly', () => {
